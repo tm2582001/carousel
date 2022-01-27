b5259045ef2ae3efa30d4b6cbe556ac395e9270c
Tushar <tm2582001@gmail.com>
2022-01-27 13:28:42 +0530

    added caption hover effect and indicator

--- index.js
+++ index.js
@@ -4,22 +4,44 @@ let classes = (classes)=>document.getElementsByClassName(classes);
 
 let displayData = (carouselData)=>{
   carouselData.map((slideData,index)=>{
-    let carouselDiv =classes("carousel-inner")[0];
+    let carouselIndicatorsDiv = classes("carousel-indicators")[0];
 
-    let Div = document.createElement("div");
-    Div.setAttribute("class","carousel-item");
+    let carouselIndicatorButton = document.createElement("button");
+    carouselIndicatorButton.setAttribute("type","button");
+    carouselIndicatorButton.setAttribute("data-bs-target","#carouselExampleIndicators");
+    carouselIndicatorButton.setAttribute("data-bs-slide-to",index);
     if(index === 0){
-      Div.classList.add("active");
+      carouselIndicatorButton.classList.add("active");
     }
-    Div.style.background = slideData.category.bgColor;
-    Div.style.color = slideData.category.color;
+
+    carouselIndicatorsDiv.appendChild(carouselIndicatorButton);
 
 
-    let H2 = document.createElement("h2");
-    H2.innerHTML = slideData.description;
+    let carouselInnerDiv =classes("carousel-inner")[0];
 
-    Div.appendChild(H2);
-    carouselDiv.appendChild(Div); 
+    let CarouselItemDiv = document.createElement("div");
+    CarouselItemDiv.setAttribute("class","carousel-item");
+    if(index === 0){
+      CarouselItemDiv.classList.add("active");
+    }
+    CarouselItemDiv.style.background = slideData.category.bgColor;
+    CarouselItemDiv.style.color = slideData.category.color;
+
+    let CarouselCaptionDiv = document.createElement("div");
+    CarouselCaptionDiv.setAttribute("class","carousel-caption d-none d-md-block");
+
+    let CarouselContentP = document.createElement("p");
+    
+    CarouselContentP.innerHTML = slideData.titleRaw;
+
+    CarouselCaptionDiv.appendChild(CarouselContentP);
+
+    let MainCarouselContentH2 = document.createElement("h2");
+    MainCarouselContentH2.innerHTML = slideData.description;
+
+    CarouselItemDiv.appendChild(MainCarouselContentH2);
+    CarouselItemDiv.appendChild(CarouselCaptionDiv);
+    carouselInnerDiv.appendChild(CarouselItemDiv); 
   });
 }
 
